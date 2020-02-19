1daf2be20c9ac7b8f041b3b3453a7ad9ea0d7807
ab3511 <59628662+ab3511@users.noreply.github.com>
2020-02-18 20:11:54 -0500

    handling form submissions

--- web_app/routes/newroutes.py
+++ web_app/routes/newroutes.py
@@ -7,3 +7,14 @@ newroutes = Blueprint("newroutes", __name__)
 def newindex():
     print("VISITING THE NEW INDEX PAGE")
     return render_template("newhomepage.html")
+
+@newroutes.route('/bookstoread', methods=["POST"])
+def request_books_to_read():
+    print("FINDING BOOKS...")
+    print("FORM DATA:", dict(request.form))
+    #return jsonify(request.form)
+
+    #product_name = request.form["product_name"]
+    #flash(f"Product '{product_name}' created successfully!", "success") # use the "success" category to correspond with twitter bootstrap alert colors
+    #return redirect("newhomepage.html")
+    return render_template("booksresults.html")
